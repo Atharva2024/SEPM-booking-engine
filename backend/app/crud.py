af18aa6a3f3fbe14af832c1ca3a24d38f5f13e48
Atharva_Dubey <atharvadubey2020@gmail.com>
2026-03-09 23:07:14 +0530

    CRUD for MVP

---
+++ backend/app/crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+import models
+import schemas
+
+def create_resource(db: Session, resource: schemas.ResourceCreate):
+    db_resource = models.Resource(**resource.dict())
+    db.add(db_resource)
+    db.commit()
+    db.refresh(db_resource)
+    return db_resource
+
+def get_resources(db: Session):
+    return db.query(models.Resource).all()
+
+def get_resource(db: Session, resource_id: int):
+    return db.query(models.Resource).filter(models.Resource.id == resource_id).first()
