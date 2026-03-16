e64908127950936833369ccafb751d94640e3649
Atharva_Dubey <atharvadubey2020@gmail.com>
2026-03-17 00:42:46 +0530

    add booking UI and availability check integration

--- frontend/script.js
+++ frontend/script.js
@@ -38,8 +38,64 @@ async function loadResources() {
         item.className = "list-group-item";
 
         item.innerText =
-            `${resource.name} | ₹${resource.price_per_hour}/hr | inventory: ${resource.total_inventory}`;
+    `ID: ${resource.id} | ${resource.name} | ₹${resource.price_per_hour}/hr | inventory: ${resource.total_inventory}`;
 
         list.appendChild(item);
     });
 }
+async function checkAvailability() {
+
+    const resourceId = document.getElementById("resourceId").value;
+    const start = document.getElementById("startTime").value;
+    const end = document.getElementById("endTime").value;
+
+    const response = await fetch(
+        `${API}/availability?resource_id=${resourceId}&start_time=${start}&end_time=${end}`
+    );
+
+    const data = await response.json();
+
+    const result = document.getElementById("availabilityResult");
+
+    if (data.available) {
+        result.innerText = " Slot Available";
+        result.style.color = "green";
+    } else {
+        result.innerText = " Slot Not Available";
+        result.style.color = "red";
+    }
+}
+
+async function createBooking() {
+
+    const booking = {
+        user_email: document.getElementById("email").value,
+        start_time: document.getElementById("startTime").value,
+        end_time: document.getElementById("endTime").value,
+        items: [
+            {
+                resource_id: parseInt(document.getElementById("resourceId").value),
+                quantity: 1
+            }
+        ]
+    };
+
+    const response = await fetch(`${API}/bookings`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(booking)
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+    alert("Booking created successfully!");
+
+    document.getElementById("email").value = "";
+    document.getElementById("resourceId").value = "";
+    document.getElementById("startTime").value = "";
+    document.getElementById("endTime").value = "";
+    document.getElementById("availabilityResult").innerText = "";
+} else {
+    alert(data.detail);
+}}
